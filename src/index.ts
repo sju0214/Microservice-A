938b333afd4fd52b408930be345cec2bfacf038c
FireIsGood <fireisadequate@gmail.com>
2025-02-28 19:16:48 -0800

    Refactor to use pokedex-promise

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 
+import Pokedex from "pokedex-promise-v2";
+const P = new Pokedex();
+
 const app = new Hono();
 
 // Get random pokemon from specified region
 app.get("/pokemon/:region", async (c) => {
   try {
     const region = c.req.param("region");
-    const response = await fetch(`https://pokeapi.co/api/v2/pokedex/${region}`);
+    const pokedex = await P.getPokedexByName(region);
 
-    const data = await response.json();
-    const pokemonList = data.pokemon_entries.map((entry: any) => entry.pokemon_species.name);
+    const pokemonList = pokedex.pokemon_entries.map((entry) => entry.pokemon_species.name);
 
     // Get random Pokemon from specified region
     const randomPokemon = pokemonList[Math.floor(Math.random() * pokemonList.length)];
-    const pokemonInfo = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`).then((res) => res.json());
+    const pokemonInfo = await P.getPokemonByName(randomPokemon);
 
     // Extract Pokemon's ID and type from info
     const id = pokemonInfo.id;
-    const pokemonType = pokemonInfo.types.map((type: any) => type.type.name);
+    const pokemonType = pokemonInfo.types.map((type) => type.type.name);
 
     // Get egg cycle of random pokemon
-    const { hatch_counter } = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${randomPokemon}`).then((res) =>
-      res.json(),
-    );
+    const { hatch_counter } = await P.getPokemonSpeciesByName(randomPokemon);
 
     return c.json({
       name: randomPokemon,
