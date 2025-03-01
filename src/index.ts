cdaa8c868d658960c70123eacc01dbbae7770c05
FireIsGood <fireisadequate@gmail.com>
2025-02-28 19:26:07 -0800

    Refactor to use inline error handling

--- src/index.ts
+++ src/index.ts
@@ -8,32 +8,32 @@ const app = new Hono();
 
 // Get random pokemon from specified region
 app.get("/pokemon/:region", async (c) => {
-  try {
-    const region = c.req.param("region");
-    const pokedex = await P.getPokedexByName(region);
+  const region = c.req.param("region");
+  const pokedex = await P.getPokedexByName(region).catch(() => undefined);
+  if (pokedex === undefined) return c.json({ error: "Region does not exist" }, 400);
 
-    const pokemonList = pokedex.pokemon_entries.map((entry) => entry.pokemon_species.name);
+  const pokemonList = pokedex.pokemon_entries.map((entry) => entry.pokemon_species.name);
 
-    // Get random Pokemon from specified region
-    const randomPokemon = pokemonList[Math.floor(Math.random() * pokemonList.length)];
-    const pokemonInfo = await P.getPokemonByName(randomPokemon);
+  // Get random Pokemon from specified region
+  const randomPokemon = pokemonList[Math.floor(Math.random() * pokemonList.length)];
 
-    // Extract Pokemon's ID and type from info
-    const id = pokemonInfo.id;
-    const pokemonType = pokemonInfo.types.map((type) => type.type.name);
+  // Get id and type from the Pokemon
+  const pokemonInfo = await P.getPokemonByName(randomPokemon).catch(() => undefined);
+  if (pokemonInfo === undefined) return c.json({ error: "Internal Server Error" }, 500);
+  const id = pokemonInfo.id;
+  const types = pokemonInfo.types.map((type) => type.type.name);
 
-    // Get egg cycle of random pokemon
-    const { hatch_counter } = await P.getPokemonSpeciesByName(randomPokemon);
+  // Get egg cycle of random Pokemon
+  const pokemonSpecies = await P.getPokemonSpeciesByName(randomPokemon).catch(() => undefined);
+  if (pokemonSpecies === undefined) return c.json({ error: "Internal Server Error" }, 500);
+  const egg_cycles = pokemonSpecies.hatch_counter;
 
-    return c.json({
-      name: randomPokemon,
-      id,
-      types: pokemonType,
-      egg_cycles: hatch_counter,
-    });
-  } catch (error) {
-    return c.json({ error: "Internal Server Error" }, 500);
-  }
+  return c.json({
+    name: randomPokemon,
+    id,
+    types,
+    egg_cycles,
+  });
 });
 
 serve(
